d4aa83f1d11084f316b2969febf7168bba07cc2a
Artur Roszczyk <artur.roszczyk@gmail.com>
2012-10-23 23:33:40 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShipitExample::Application.config.secret_token = 'b0d968c4aaad3385828ee97d69c918712e2a248b01a9e1e3e5a318bb32b5e7aef9cd2d7dd6301de097cf386b1ac8df7ee0f3fb3282c5f90a60d009dacac13101'
